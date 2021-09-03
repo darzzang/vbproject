eebfed3d3fc9d7312f4a6e36c84c4dd131de7224
Haeng <85557392+ShinHyoHaeng@users.noreply.github.com>
2021-09-03 16:35:29 +0900

    예약자 DB 테이블 생성

---
+++ myVaccine/WebContent/resources/sql/vac_database.sql
@@ -0,0 +1,48 @@
+drop table vac;
+drop table data;
+
+create table if not exists vac(
+	v_vaccine varchar(10) not null,
+	v_name varchar(10),
+	v_idNum1 varchar(10),
+	v_idNum2 varchar(10),
+	v_phone1 varchar(10),
+	v_phone2 varchar(10),
+	v_phone3 varchar(10),
+	primary key(v_name)
+)default CHARSET=utf8;
+
+create table if not exists data(
+	v_vaccine varchar(10) not null,
+	v_name varchar(10),
+	v_idNum1 varchar(10),
+	v_idNum2 varchar(10),
+	v_phone1 varchar(10),
+	v_phone2 varchar(10),
+	v_phone3 varchar(10),
+	v_idNum varchar(10),
+	v_phone varchar(20),
+	primary key(v_phone)
+)default CHARSET=utf8;
+
+create table if not exists check(
+	v_vaccine varchar(10) not null,
+	v_name varchar(10),
+	v_idNum1 varchar(10),
+	v_idNum2 varchar(10),
+	v_phone1 varchar(10),
+	v_phone2 varchar(10),
+	v_phone3 varchar(10),
+	v_idNum varchar(10),
+	v_phone varchar(20),
+	primary key(v_phone)
+)default CHARSET=utf8;
+
+select * from data where v_phone = '11111111111';
+insert into vac values('pakf');
+select * from vac;
+select * from data;
+select v_phone from data;
+show tables;
+insert into check from data where v_phone = '11111111111';
+desc vac;
