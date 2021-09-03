7843b4a2abb73ba637725d020f9df43a0bdae6d0
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-03 14:53:05 +0900

    유효성 검사 스크립트 수정(이름 체크 부분만 수정)

--- myVaccine/WebContent/resources/js/validation.js
+++ myVaccine/WebContent/resources/js/validation.js
@@ -1,16 +1,27 @@
+
+// 함수명 추가
+function checkBooking(){
+	
+	// '이름' 변수 선언
+	var name = document.getElementById("name");
+	
 	var idNum2 = document.getElementById("idNum2");
 	var phone1 = document.getElementById("phone1");
 	var phone2 = document.getElementById("phone2");
 	var phone3 = document.getElementById("phone3");
+	
+	/* Ch.08 유효성 검사.pdf 3. 데이터 형식 유효성 검사 예제 8-5 활용 */
+	var regExpName = /^[가-힣]*$/;
+	
 	
 	//이름 체크
 	if (name=="" || name==null) {
 		alert("이름을 입력하세요.");
 		name.select();
 		name.focus();
-		return false;
-	}
-	else if (!check(/^[가-힣]/$, name, "이름은 한글만 사용가능합니다.")) {
+		return false();
+	}else if (!regExpName.test(name)) { // 유효성 검사 형식 수정
+		alert("이름은 한글만 사용가능합니다.");
 		return false;
 	}
 	
@@ -54,5 +65,7 @@
 		e.focus();
 		return false;
 	}
+	
 	document.result.submit() 
+	
 }
