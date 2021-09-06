a1d77ec64fd35eb6d63a37179a1bf266891d5022
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-06 10:33:59 +0900

    의료기관DB에 있던 백신재고량 항목 삭제 기관 이름(p_instName)을 primary key로 지정

--- myVaccine/WebContent/resources/sql/institution.sql
+++ myVaccine/WebContent/resources/sql/institution.sql
@@ -1,3 +1,5 @@
+drop table institution;
+
 create table if not exists institution(
 	p_appDate DATE,
 	p_instAddress1 VARCHAR(50),
@@ -6,14 +8,14 @@ create table if not exists institution(
 	p_instAddress4 VARCHAR(50),
 	p_instName VARCHAR(50),
 	p_instPhone VARCHAR(20),
-	p_instWorkHour TEXT,
-	p_modslnStock LONG,
-	p_pfislnStock LONG
+	p_instWorkHour TEXT
 )default charset=utf8;
-
 
 select * from institution;
 
-/* 테스트를 위한 데이터 입력 */
-insert into institution values('2021-09-03', '대구광역시', '동구', '동촌로', '79', '동구보건소','053-662-3201','평일 09:00 - 18:00',30, 30);
+/* 기관 이름(p_instName)을 primary key로 지정 */
+alter table institution add primary key(p_instName);
 
+/* 테스트를 위한 데이터 입력 */
+/*insert into institution values('2021-09-03', '대구광역시', '동구', '동촌로', '79', '동구보건소','053-662-3201','평일 09:00 - 18:00',30, 30);*/
+insert into institution values('2021-09-03', '대구광역시', '동구', '동촌로', '79', '동구보건소','053-662-3201','평일 09:00 - 18:00');
