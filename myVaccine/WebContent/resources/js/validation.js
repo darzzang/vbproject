d48c8f2b5ddbcec4961a24a891f64fcc2885a8ad
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-09 13:29:12 +0900

    안내문구 출력 방식 변경
    alert() 대신 각각의 안내문구를 담은 p 태그를 보여주는 방식으로 변경

--- myVaccine/WebContent/resources/js/validation.js
+++ myVaccine/WebContent/resources/js/validation.js
@@ -1,6 +1,8 @@
 
-function checkBooking(){
 	
+/* booking.jsp 내 form 유효성 검사 */
+function checkBooking(){
+
 	var name = newBooking.name.value;
 	var idNum1 = newBooking.idNum1.value;
 	var idNum2 = newBooking.idNum2.value;
@@ -8,8 +10,6 @@ function checkBooking(){
 	var phone2 = newBooking.phone2.value;
 	var phone3 = newBooking.phone3.value;
 	
-	
-	/* Ch.08 유효성 검사.pdf 3. 데이터 형식 유효성 검사 예제 8-5 활용 */
 	var regExpName = /^[가-힣]+$/; 
 	var regExpBirth = /^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))/;
 	var regExpGen = /^[1-4]{1}/;
@@ -17,11 +17,11 @@ function checkBooking(){
 	var regExpPhn2 = /^(?:\d{3}|\d{4})/;
 	var regExpPhn3 = /^\d{4}/;
 	
-	
 	//이름 체크
 	if (!regExpName.test(name)) {
 		console.log("test1: 이름 체크");
-		alert("이름은 한글만 사용 가능합니다.");
+		$(".alertText").hide();
+		$("#nameError").fadeIn();
 		name = "";
 		newBooking.name.focus();
 		return;
@@ -30,7 +30,8 @@ function checkBooking(){
 	//주민등록번호 앞자리 체크
 	if (!regExpBirth.test(idNum1)) {
 		console.log("test2: 주민등록번호 앞자리 체크");
-		alert("주민등록번호 앞자리는 6개의 숫자만 입력 가능합니다.");
+		$(".alertText").hide();
+		$("#birthError").fadeIn();
 		idNum1 = "";
 		newBooking.idNum1.focus();
 		return;
@@ -39,7 +40,8 @@ function checkBooking(){
 	//주민등록번호 뒷자리 체크
 	if (!regExpGen.test(idNum2)) { 
 		console.log("test3: 주민등록번호 뒷자리 체크");
-		alert("주민등록번호 뒷자리는 1,2,3,4만 가능합니다.");
+		$(".alertText").hide();
+		$("#genError").fadeIn();
 		idNum2 = "";
 		newBooking.idNum2.focus();
 		return;
@@ -48,26 +50,40 @@ function checkBooking(){
 	//휴대폰번호 체크
 	if(!regExpPhn1.test(phone1)) {
 		console.log("test4: 휴대폰 번호 앞자리 체크");
-		alert("휴대폰 번호를 정확하게 입력하세요.");
+		$(".alertText").hide();
+		$("#phnError").fadeIn();
 		phone1 = "";
 		newBooking.phone1.focus();
 		return;
 	}
 	if(!regExpPhn2.test(phone2)) {
 		console.log("test5: 휴대폰 번호 중간자리 체크");
-		alert("휴대폰 번호를 정확하게 입력하세요.");
+		$(".alertText").hide();
+		$("#phnError").fadeIn();
 		phone2 = "";
 		newBooking.phone2.focus();
 		return;
 	}
 	if(!regExpPhn3.test(phone3)) {
 		console.log("test6: 휴대폰 번호 뒷자리 체크");
-		alert("휴대폰 번호를 정확하게 입력하세요.");
+		$(".alertText").hide();
+		$("#phnError").fadeIn();
 		phone3 = "";
 		newBooking.phone3.focus();
 		return;
 	}
 	
-	document.newBooking.submit(); 
+	document.newBooking.submit();
 	
+}
+
+
+/* modal 검색영역 유효성 검사 */
+function findInstitution(){
+	
+	var date = findInst.date.value;
+
+	var holiday = 
+
+	document.findInst.submit(); 
 }
