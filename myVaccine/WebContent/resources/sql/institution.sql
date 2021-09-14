925acad82e7bcec22db190acb39f61fbfb54e322
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-09-14 15:03:19 +0900

    의료기관 DB와 백신 DB를 통합한 instTBL 추가

--- myVaccine/WebContent/resources/sql/institution.sql
+++ myVaccine/WebContent/resources/sql/institution.sql
@@ -1,7 +1,7 @@
 drop table institution;
 
 create table if not exists institution(
-	p_appDate DATE,
+	p_instID int(10) not null auto_increment primary key, 
 	p_instAddress1 VARCHAR(50),
 	p_instAddress2 VARCHAR(50),
 	p_instAddress3 VARCHAR(50),
@@ -17,10 +17,11 @@ select * from institution;
 alter table institution add primary key(p_instName);
 
 /* 테스트를 위한 데이터 입력 */
-insert into institution values('2021-09-03', '대구광역시', '동구', '검사동', '1005-8', '동구보건소','053-662-3201','평일 09:00 - 18:00. 주말 및 공휴일 휴무');
-insert into institution values('2021-09-03', '대구광역시', '동구', '효목동', '1084', '동구 예방접종센터','053-951-3300','평일 09:00 - 18:00. 주말 및 공휴일 휴무');
+insert into institution values(1, '대구광역시', '동구', '검사동', '1005-8', '동구보건소','053-662-3201','평일 09:00 - 18:00. 주말 및 공휴일 휴무');
+insert into institution values(2, '대구광역시', '동구', '효목동', '1084', '동구 예방접종센터','053-951-3300','평일 09:00 - 18:00. 주말 및 공휴일 휴무');
 
-/* institution table + vaccine table */
-SELECT * FROM institution JOIN vaccine ON institution.p_instName=vaccine.vac_inst;
+/* instTBL = institution table + vaccine table */
+CREATE TABLE instTBL SELECT * FROM institution JOIN vaccine ON institution.p_instID=vaccine.vac_instCode;
+select * from instTBL;
 
-
+select * from  institution where p_instAddress3 = '검사동';
