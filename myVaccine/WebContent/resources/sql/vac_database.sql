3797eeba8b2b4d4314b484f36c85f23c784f5988
JHooLee <avrilsmile@naver.com>
2021-09-27 16:14:19 +0900

    데이터베이스 변동에 의해 Edit영역이 변함 bookingEdit : 예약 페이지(booking)와 양식 통일 processEditing : 수정 과정도 예약 과정처럼 1단계 2단계로 나뉘는데 2단계 수정 중 취소할 경우 1단계에서 수정했던 부분을 원래대로 되돌리기 위해 코드 바꿈 수정 1단계에서 2단계로 가는 부분에서만 사용할 테이블 edit 생성 후 사용 vac_database : edit 테이블 생성

--- myVaccine/WebContent/resources/sql/vac_database.sql
+++ myVaccine/WebContent/resources/sql/vac_database.sql
@@ -1,8 +1,31 @@
 drop table vac;
 drop table data;
 drop table bigdata;
+drop table edit;
 
 create table if not exists vac(
+	v_vaccine varchar(10) not null,
+	v_name varchar(10),
+	v_idNum1 varchar(10),
+	v_idNum2 varchar(10),
+	v_phone1 varchar(10),
+	v_phone2 varchar(10),
+	v_phone3 varchar(10),
+	v_idNum varchar(10),
+	v_phone varchar(20),
+	p_appDate DATE, 
+	p_instAddress1 VARCHAR(50),
+	p_instAddress2 VARCHAR(50),
+	p_instAddress3 VARCHAR(50),
+	p_instAddress4 VARCHAR(50),
+	p_instName VARCHAR(50),
+	p_instPhone VARCHAR(20),
+	p_instWorkHour TEXT,
+	primary key(v_name)
+)default CHARSET=utf8;
+
+--예약 수정 페이지에서 수정 중 중간에 취소할 경우를 대비하기 위해 사용하는 임시 테이블.
+create table if not exists edit(
 	v_vaccine varchar(10) not null,
 	v_name varchar(10),
 	v_idNum1 varchar(10),
@@ -64,6 +87,7 @@ insert into vac values('pakf');
 select * from vac;
 select * from data;
 select * from bigdata;
+select * from edit;
 select v_phone from data where v_phone = '01111111111';
 select v_phone from vac;
 select v_phone from data where v_name ="가가가";
